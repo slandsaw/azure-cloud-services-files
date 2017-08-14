a34d2bc45f65ff95dc24326b3966386d9563588d
slandsaw <slandsaw@greenspacemeetings.com>
2017-08-14 19:31:52 -0400

    Add files via upload

---
+++ install-adalsql.ps1
@@ -0,0 +1,13 @@
+﻿$webclient = New-Object System.Net.WebClient
+
+$webclient.DownloadFile("https://download.microsoft.com/download/6/4/6/64677D6E-06EA-4DBB-AF05-B92403BB6CB9/ENU/x64/adalsql.msi","$HOME\Downloads\adalsql.msi")
+
+cd "$HOME\Downloads"
+
+$msifile = "$HOME\Downloads\adalsql.msi"
+
+$arguments= ' /qn /l*v .\install_adalsql.txt' 
+Start-Process `
+     -file  "$msifile" `
+     -arg $arguments `
+     -passthru | wait-process
