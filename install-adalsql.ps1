0d86f039e578ef095cde0d983915296fbfed9f0c
slandsaw <slandsaw@greenspacemeetings.com>
2017-08-14 20:11:53 -0400

    Update file

--- install-adalsql.ps1
+++ install-adalsql.ps1
@@ -1,10 +1,8 @@
-﻿$webclient = New-Object System.Net.WebClient
+$webclient = New-Object System.Net.WebClient
 
-$webclient.DownloadFile("https://download.microsoft.com/download/6/4/6/64677D6E-06EA-4DBB-AF05-B92403BB6CB9/ENU/x64/adalsql.msi","$HOME\Downloads\adalsql.msi")
+$webclient.DownloadFile("https://download.microsoft.com/download/6/4/6/64677D6E-06EA-4DBB-AF05-B92403BB6CB9/ENU/x64/adalsql.msi","adalsql.msi")
 
-cd "$HOME\Downloads"
-
-$msifile = "$HOME\Downloads\adalsql.msi"
+$msifile = "adalsql.msi"
 
 $arguments= ' /qn /l*v .\install_adalsql.txt' 
 Start-Process `
